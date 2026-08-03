6fb7c5a6c46bb18e2243269534fe8eac8e59c137
mani-dev23 <manikandan23.dev@gmail.com>
2026-08-03 17:43:03 +0530

    Day 07 Optimize LC0001 Two Sum using HashMap

--- DSA/Day06/LC0001_TwoSum.py
+++ DSA/Day06/LC0001_TwoSum.py
@@ -7,10 +7,6 @@ Approach 1: Brute Force
 Time Complexity: O(n²)
 Space Complexity: O(1) 
 
-Approach 2: Hash Map
-Time Complexity: O(n)
-Space Complexity: O(n)
-
 """
 
 class SolutionBruteForce(object):
@@ -25,20 +21,3 @@ brute = SolutionBruteForce()
 
 print(brute.twoSum([2, 7, 11, 15], 9))
 
-
-
-class SolutionHashMap(object):
-    def twoSum(self, nums, target):
-        hashmap = {}
-
-        for i, num in enumerate(nums):
-            need = target - num
-
-            if need in hashmap:
-                return [hashmap[need], i]
-
-            hashmap[num] = i
-
-optimal = SolutionHashMap()
-
-print(optimal.twoSum([2, 7, 11, 15], 9))
--- DSA/Day07/LC0001_TwoSum.py
+++ DSA/Day07/LC0001_TwoSum.py
@@ -2,29 +2,11 @@
 LeetCode #1
 Problem: Two Sum
 
-
-Approach 1: Brute Force
-Time Complexity: O(n²)
-Space Complexity: O(1) 
-
 Approach 2: Hash Map
 Time Complexity: O(n)
 Space Complexity: O(n)
 
 """
-
-class SolutionBruteForce(object):
-    def twoSum(self, nums, target):
-
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-
-brute = SolutionBruteForce()
-
-print(brute.twoSum([2, 7, 11, 15], 9))
-
 
 
 class SolutionHashMap(object):
